de3a91db14bf0144d7a2594b858d228adf369632
VISHRUT86 <vishrut8585@gmail.com>
2025-07-22 15:55:57 +0530

    mongo db connection

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => console.log("MongoDB connected"))
   .catch(err => console.log("MongoDB connection error:", err));
 
